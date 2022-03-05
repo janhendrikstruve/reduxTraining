fa44def75134808f9f3e9d541c5ccccc6df3fa3a
Jan <janhendrikstruve@gmail.com>
2022-03-05 14:29:29 +0100

    :art: Refactor: swap out createreducer and createactions with createslice for better readebility

--- src/app/reduxTest.js
+++ src/app/reduxTest.js
@@ -5,6 +5,8 @@ export default function reduxTest() {
   const store = configureStore();
 
   store.dispatch(actions.bugAdded({ description: 'Bug1' }));
+  store.dispatch(actions.bugAdded({ description: 'Bug2' }));
+  store.dispatch(actions.bugAdded({ description: 'Bug3' }));
   store.dispatch(actions.bugResolved({ id: 1 }));
 
   console.log(store.getState());
--- src/app/store/bugs.js
+++ src/app/store/bugs.js
@@ -1,29 +1,28 @@
-import { createAction, createReducer } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-//Action Creators
-export const bugAdded = createAction('bugAdded');
-export const bugResolved = createAction('bugResolved');
-export const bugRemoved = createAction('bugRemoved');
-
-// Reducer
 let lastId = 0;
 
-export default createReducer([], {
-  // key: value
-  // actions: functions (event => event handler)
-  [bugAdded.type]: (bugs, action) => {
-    bugs.push({
-      id: ++lastId,
-      description: action.payload.description,
-      resolved: false,
-    });
-  },
-  [bugResolved.type]: (bugs, action) => {
-    bugs.map((bug) =>
-      bug.id === action.payload.id ? (bug.resolved = true) : bug
-    );
-  },
-  [bugRemoved.type]: (bugs, action) => {
-    bugs.filter((bug) => bug.id !== action.payload.id);
+const slice = createSlice({
+  name: 'bugs',
+  initialState: [],
+  reducers: {
+    bugAdded: (bugs, action) => {
+      bugs.push({
+        id: ++lastId,
+        description: action.payload.description,
+        resolved: false,
+      });
+    },
+    bugResolved: (bugs, action) => {
+      bugs.map((bug) =>
+        bug.id === action.payload.id ? (bug.resolved = true) : bug
+      );
+    },
+    bugRemoved: (bugs, action) => {
+      bugs.filter((bug) => bug.id !== action.payload.id);
+    },
   },
 });
+
+export const { bugAdded, bugResolved } = slice.actions;
+export default slice.reducer;
