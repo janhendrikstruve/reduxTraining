ec1cefb2957946946c470ff5f16d529f687e7bb1
Jan <janhendrikstruve@gmail.com>
2022-03-05 15:33:30 +0100

    :sparkles: Add: Add new reducer for projects and use combinereducers to combine them

--- src/app/reduxTest.js
+++ src/app/reduxTest.js
@@ -1,13 +1,17 @@
 import configureStore from './store/configureStore';
-import * as actions from './store/bugs';
+import { projectAdded } from './store/projects';
+import { bugAdded, bugResolved } from './store/bugs';
 
 export default function reduxTest() {
   const store = configureStore();
 
-  store.dispatch(actions.bugAdded({ description: 'Bug1' }));
-  store.dispatch(actions.bugAdded({ description: 'Bug2' }));
-  store.dispatch(actions.bugAdded({ description: 'Bug3' }));
-  store.dispatch(actions.bugResolved({ id: 1 }));
+  store.dispatch(projectAdded({ name: 'project1' }));
+  store.dispatch(projectAdded({ name: 'project2' }));
+  store.dispatch(projectAdded({ name: 'project3' }));
+  store.dispatch(bugAdded({ description: 'Bug1' }));
+  store.dispatch(bugAdded({ description: 'Bug2' }));
+  store.dispatch(bugAdded({ description: 'Bug3' }));
+  store.dispatch(bugResolved({ id: 1 }));
 
   console.log(store.getState());
 }
