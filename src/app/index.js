a3f3159c4d6b51e1d72b2d7f0cd5b38002dba809
Jan <janhendrikstruve@gmail.com>
2022-03-06 17:36:34 +0100

    :wastebasket: Cleaning: Cleane up index.js for working with the middleware

--- src/app/index.js
+++ src/app/index.js
@@ -1,30 +1,5 @@
 import configureStore from './store/configureStore';
-import { projectAdded } from './store/projects';
-import {
-  bugAdded,
-  bugResolved,
-  bugAssignedToUser,
-  getBugsByUser,
-} from './store/bugs';
-import { userAdded } from './store/users';
 
 export default function index() {
   const store = configureStore();
-
-  store.dispatch(userAdded({ name: 'Olaf' }));
-
-  store.dispatch(projectAdded({ name: 'project1' }));
-  store.dispatch(projectAdded({ name: 'project2' }));
-  store.dispatch(projectAdded({ name: 'project3' }));
-
-  store.dispatch(bugAdded({ description: 'Bug1' }));
-  store.dispatch(bugAdded({ description: 'Bug2' }));
-  store.dispatch(bugAdded({ description: 'Bug3' }));
-  store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
-  store.dispatch(bugResolved({ id: 1 }));
-
-  console.log(store.getState());
-
-  const bugs = getBugsByUser(1)(store.getState());
-  console.log(bugs);
 }
