7b3827b596053423c49f4786db1bace10fa062df
Jan <janhendrikstruve@gmail.com>
2022-03-06 11:30:19 +0100

    :sparkles: Add: Add ability to assisiate bugs to a user

--- src/app/store/bugs.js
+++ src/app/store/bugs.js
@@ -22,13 +22,26 @@ const slice = createSlice({
     bugRemoved: (bugs, action) => {
       bugs.filter((bug) => bug.id !== action.payload.id);
     },
+    bugAssignedToUser: (bugs, action) => {
+      bugs.map((bug) =>
+        bug.id === action.payload.bugId
+          ? (bug.users = action.payload.userId)
+          : bug
+      );
+    },
   },
 });
 
-export const { bugAdded, bugResolved } = slice.actions;
+export const { bugAdded, bugResolved, bugAssignedToUser } = slice.actions;
 export default slice.reducer;
 
 export const getUnresolvedBugs = createSelector(
   (state) => state.entities.bugs,
   (bugs) => bugs.filter((bug) => !bug.resolved)
 );
+
+export const getBugsByUser = (userId) =>
+  createSelector(
+    (state) => state.entities.bugs,
+    (bugs) => bugs.filter((bug) => bug.userId === userId)
+  );
